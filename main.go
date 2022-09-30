52ed60a0c5e8e2dadb1057f63e5ec3d1d91da9d3
Raphael Rail <raphthearrow@gmail.com>
2022-09-30 08:15:21 -0400

    Made loadHome work, added loadClient, loadTech and load404

--- main.go
+++ main.go
@@ -1,5 +1,93 @@
 package main
 
+import (
+	"github.com/gorilla/websocket"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var upgrader = websocket.Upgrader{}
+
 func main() {
-	println("Hello, World!")
+	http.HandleFunc("/", loadHome)
+	http.HandleFunc("/client", loadClient)
+	http.HandleFunc("/ws", ws)
+
+	fileServer := http.FileServer(http.Dir("./src/assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fileServer))
+
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func loadHome(w http.ResponseWriter, r *http.Request) {
+
+	vue, _ := os.ReadFile("./src/views/template.html")
+	vueStr := string(vue)
+	home, _ := os.ReadFile("./src/views/home.html")
+	homeStr := string(home)
+	vueStr = strings.Replace(vueStr, "###TITLE###", "Clavardage du C.A.I.", 1)
+	vueStr = strings.Replace(vueStr, "###SUBTITLE###", "On vous aide même à distance !", 1)
+	vueStr = strings.Replace(vueStr, "###CONTENT###", homeStr, 1)
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, vueStr)
+}
+
+func loadClient(w http.ResponseWriter, r *http.Request) {
+
+	vue, _ := os.ReadFile("./src/views/template.html")
+	vueStr := string(vue)
+	home, _ := os.ReadFile("./src/views/client.html")
+	homeStr := string(home)
+	vueStr = strings.Replace(vueStr, "###TITLE###", "Clavardage du C.A.I.", 1)
+	vueStr = strings.Replace(vueStr, "###SUBTITLE###", "Attendez, un technicien est sur le point de vous aider !", 1)
+	vueStr = strings.Replace(vueStr, "###CONTENT###", homeStr, 1)
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, vueStr)
+}
+
+func loadTech(w http.ResponseWriter, r *http.Request) {
+
+	vue, _ := os.ReadFile("./src/views/template.html")
+	vueStr := string(vue)
+	home, _ := os.ReadFile("./src/views/tech.html")
+	homeStr := string(home)
+	vueStr = strings.Replace(vueStr, "###TITLE###", "Clavardage du C.A.I.", 1)
+	vueStr = strings.Replace(vueStr, "###SUBTITLE###", "Aidez le plus de personne possible !", 1)
+	vueStr = strings.Replace(vueStr, "###CONTENT###", homeStr, 1)
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, vueStr)
+}
+
+func load404(w http.ResponseWriter, r *http.Request) {
+
+	vue, _ := os.ReadFile("./src/views/template.html")
+	vueStr := string(vue)
+	home, _ := os.ReadFile("./src/views/404.html")
+	homeStr := string(home)
+	vueStr = strings.Replace(vueStr, "###TITLE###", "Clavardage du C.A.I.", 1)
+	vueStr = strings.Replace(vueStr, "###SUBTITLE###", "Page introuvable", 1)
+	vueStr = strings.Replace(vueStr, "###CONTENT###", homeStr, 1)
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, vueStr)
+}
+
+func ws(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+	}
+	defer conn.Close()
+	for {
+		mt, msg, _ := conn.ReadMessage()
+		log.Printf("recv: %s", msg)
+		conn.WriteMessage(mt, msg)
+	}
 }
